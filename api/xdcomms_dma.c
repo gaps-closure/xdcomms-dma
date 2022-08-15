d6b298feb9a25804cc9adac0eb66b81eb90d5dc6
Michael Kaplan <mkaplan@perspectalabs.com>
2022-08-15 14:41:30 +0000

    semi colon

--- api/xdcomms_dma.c
+++ api/xdcomms_dma.c
@@ -367,7 +367,7 @@ int open_channel(chan *c, const char **channel_name, int channel_count, int buff
 #ifdef SHARED_MEMORY_MODE
     c[i].fd = open(channel_name[i], O_RDWR);
 #else
-    char dev_chan[64] = "/dev/"
+    char dev_chan[64] = "/dev/";
     if (strlen(channel_name[i]) >= 60) {
       log_fatal("Channel name must be less than 60 chars: %s", channel_name[i]);
       exit(EXIT_FAILURE);
