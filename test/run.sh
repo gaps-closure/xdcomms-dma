7c8a40a9eca5d06dd5c3023c83dfe9e0808fab50
Rajesh Krishnan <rjsh.krshnn@gmail.com>
2022-08-23 11:22:40 -0400

    makefile for local testing on x86 with pseudo driver

--- test/run.sh
+++ test/run.sh
@@ -1,15 +1,9 @@
 cd ../pseudo 
-if !(test -f sue_donimous.ko)
-then
-  make
-fi
+if !(test -f sue_donimous.ko) then make; fi
 sudo ./sue_donimous_unload >& /dev/null
 sudo ./sue_donimous_load
 cd ../test
-if !(test -f dma-proxy-test)
-then
-  make
-fi
+if !(test -f dma-proxy-test) then make; fi
 DMATXDEV=sue_donimous_tx0 DMARXDEV=sue_donimous_rx1 ./dma-proxy-test 8 64 1 &
 DMATXDEV=sue_donimous_tx1 DMARXDEV=sue_donimous_rx0 ./dma-proxy-test 8 64 1 &
 make clean
