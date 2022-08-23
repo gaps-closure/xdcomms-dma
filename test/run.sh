8704103e2b479b19a40e4227d1f6f5a4daff7f24
Rajesh Krishnan <rjsh.krshnn@gmail.com>
2022-08-23 13:09:46 -0400

    fix rx/tx topology

--- test/run.sh
+++ test/run.sh
@@ -4,8 +4,8 @@ sudo ./sue_donimous_unload >& /dev/null
 sudo ./sue_donimous_load
 cd ../test
 if !(test -f dma-proxy-test) then make; fi
-DMATXDEV=sue_donimous_tx0 DMARXDEV=sue_donimous_rx1 ./dma-proxy-test 8 64 1 &
-DMATXDEV=sue_donimous_tx1 DMARXDEV=sue_donimous_rx0 ./dma-proxy-test 8 64 1 &
+DMATXDEV=sue_donimous_tx0 DMARXDEV=sue_donimous_rx0 ./dma-proxy-test 8 64 1 &
+DMATXDEV=sue_donimous_tx1 DMARXDEV=sue_donimous_rx1 ./dma-proxy-test 8 64 1 &
 make clean
 cd ../pseudo 
 sudo ./sue_donimous_unload
