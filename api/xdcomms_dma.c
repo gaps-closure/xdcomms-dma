427c59076c247b43fde9defe9cd9f29368239593
tmcauley <amcauley@peratonlabs.com>
2023-04-13 16:25:13 -0400

    simple test program for xdcomms (from hal/test)

--- api/xdcomms_dma.c
+++ api/xdcomms_dma.c
@@ -279,7 +279,7 @@ int send_channel_buffer(chan *c, size_t packet_len, int buffer_id) {
 }
 
 void get_tx_dev_name_and_type(char *dev_type, char *tx_channel_name) {
-  char *type = getenv("TYPEDEV");
+  char *type = getenv("TYPEDEV"), *tx = NULL;
   
   log_trace("XX %s", type);
   if (type==NULL) strcpy(dev_type, "dma");
