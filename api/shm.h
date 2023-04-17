8d06f54e759684bda3eff485978188820def3a58
tmcauley <amcauley@peratonlabs.com>
2023-04-17 19:32:29 -0400

    support shm and dma

--- api/shm.h
+++ api/shm.h
@@ -36,8 +36,8 @@ typedef struct _pdata {
 /* SHM channel (one per TAG) */
 typedef struct shm_channel {
   cinfo  cinfo;
-  pinfo  pinfo[SHM_CHANNEL_PKT_COUNT];
-  pdata  pdata[SHM_CHANNEL_PKT_COUNT];
+  pinfo  pinfo[SHM_MAX_INDEX];
+  pdata  pdata[SHM_MAX_INDEX];
   int       next_pkt_index;                 // Index to next packet to be written
 } shm_channel __attribute__ ((aligned (1024)));		/*  byte alignment */
 
