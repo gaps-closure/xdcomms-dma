e78ffa999f64e3ccb1415fcb43c1fd4f78343e30
tmcauley <amcauley@peratonlabs.com>
2023-04-22 13:49:09 -0400

    support shm and dma

--- api/xdcomms.c
+++ api/xdcomms.c
@@ -652,7 +652,7 @@ int  xdc_recv(void *socket, void *adu, gaps_tag *tag) {
 //  time_trace("XDC_Rx1 Wait for tag=<%d,%d,%d> (test %d times every %d ns)", tag->mux, tag->sec, tag->typ, ntries, RX_POLL_INTERVAL_NSEC);
   while ((ntries--) > 0)  {
     if (nonblock_recv(adu, tag, cp) > 0)  return 0;
-//    log_trace("LOOP timeout %s: tag=<%d,%d,%d>: remaining tries = %d ", __func__, tag->mux, tag->sec, tag->typ, ntries);
+    log_trace("LOOP timeout %s: tag=<%d,%d,%d>: remaining tries = %d ", __func__, tag->mux, tag->sec, tag->typ, ntries); exit(22);
     nanosleep(&request, NULL);
   }
   log_trace("%s timeout for tag=<%d,%d,%d>", __func__, tag->mux, tag->sec, tag->typ);
