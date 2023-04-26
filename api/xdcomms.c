1c35589cd754e9c21eb632ba61e55341f9c92e43
tmcauley <amcauley@peratonlabs.com>
2023-04-26 13:54:18 -0400

    support shm and dma

--- api/xdcomms.c
+++ api/xdcomms.c
@@ -168,6 +168,7 @@ void ctag_decode(uint32_t *ctag, gaps_tag *tag) {
 /**********************************************************************/
 void chan_print(chan *cp) {
   log_trace("cp=%p", cp);
+  log_trace("c%08x: dir=%c typ=%s", cp->ctag, cp->dev_type, cp->dev_name);
   exit(22);
   log_trace("c%08x: dir=%c typ=%s nam=%s fd=%d loc=%d", cp->ctag, cp->dev_type, cp->dev_name, cp->fd, cp->lock);
   log_trace("                  mmap len=0x%x [paddr=0x%x vaddr=%p offset=0x%x protect=0x%x flags=0x%x]",  cp->mm.len, cp->mm.phys_addr, cp->mm.virt_addr, cp->mm.offset, cp->mm.prot, cp->mm.flags);
