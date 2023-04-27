bdb97b6fab50b4c4c69642a01c5242c326f43433
tmcauley <amcauley@peratonlabs.com>
2023-04-27 17:42:54 -0400

    support shm and dma

--- api/xdcomms.c
+++ api/xdcomms.c
@@ -330,7 +330,9 @@ void shm_init_config_one(chan *cp) {
   cp->shm_addr = cp->mm.virt_addr + cp->mm.offset;
   log_trace("%s: va=%p + off=%lx = %lx", __func__, cp->mm.virt_addr, cp->mm.offset, cp->shm_addr);
 
-  cp->shm_addr->next_pkt_index           = 0;
+  cp->shm_addr.next_pkt_index           = 0;
+  log_trace("%s: index=%d", __func__, cp->shm_addr.next_pkt_index);
+
   exit(22);
   cp->shm_addr->cinfo.ctag               = cp->ctag;
   cp->shm_addr->cinfo.ms_guard_time_aw   = DEFAULT_MS_GUARD_TIME_AW;
