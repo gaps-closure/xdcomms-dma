4ba855f979aa8af4923b882a0b3e5d609c1e99bd
tmcauley <amcauley@peratonlabs.com>
2023-04-28 16:48:03 -0400

    support shm and dma

--- api/xdcomms.c
+++ api/xdcomms.c
@@ -539,8 +539,8 @@ void naive_memcpy(unsigned long *d, const unsigned long *s, unsigned long len_in
 void rcvr_shm(chan *cp, int buffer_id) {
   static int pkt_index=0;
   
-  log_trace("XXXXX", __func__);
-log_trace("THREAD-2 %s waiting for packet: pkt_index = {r=%d t=%d}", __func__, pkt_index, cp->shm_addr->pkt_index_next);
+  log_trace("XXXXX");
+//log_trace("THREAD-2 %s waiting for packet: pkt_index = {r=%d t=%d}", __func__, pkt_index, cp->shm_addr->pkt_index_next);
   chan_print (cp);
   
   
