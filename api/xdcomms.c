971705698a56f906439e618e2fbf7679ebedfa98
tmcauley <amcauley@peratonlabs.com>
2023-05-07 15:18:29 -0400

    support shm and dma

--- api/xdcomms.c
+++ api/xdcomms.c
@@ -761,6 +761,7 @@ char *xdc_set_in(char *addr_in) { return NULL; }
 char *xdc_set_out(char *addr_in) { return NULL; }
 void *xdc_ctx(void) { return NULL; }
 void *xdc_pub_socket(void) {
+  log_debug(" %s", __func__);
   gaps_tag   tag;
   char      *mp = getenv("TAG_MUX");
   char      *sp = getenv("TAG_SEC");
