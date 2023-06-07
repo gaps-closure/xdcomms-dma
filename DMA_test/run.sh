cbac17ad5dd6f0b7a636087b1ab3e2096255b66b
tmcauley <amcauley@peratonlabs.com>
2023-06-07 13:17:06 -0400

    DMA debug

--- DMA_test/run.sh
+++ DMA_test/run.sh
@@ -1,14 +1,15 @@
-cd ../pseudo 
+pushd ../xdcomms-dma/pseudo
 if !(test -f sue_donimous.ko) then make; fi
 sudo ./sue_donimous_unload >& /dev/null
 sudo ./sue_donimous_load
-cd ../test
+popd
 if !(test -f dma-proxy-test) then make; fi
-DMATXDEV=sue_donimous_tx0 DMARXDEV=sue_donimous_rx0 ./dma-proxy-test 8 64 1 &
-DMATXDEV=sue_donimous_tx1 DMARXDEV=sue_donimous_rx1 ./dma-proxy-test 8 64 1 &
+#DMATXDEV=sue_donimous_tx0 DMARXDEV=sue_donimous_rx0 ./dma-proxy-test 8 64 1 &
+#DMATXDEV=sue_donimous_tx1 DMARXDEV=sue_donimous_rx1 ./dma-proxy-test 8 64 1 &
+DMATXDEV=sue_donimous_tx1 DMARXDEV=sue_donimous_rx1 ./dma-proxy-test 1 1 1 &
 sleep 1
 make clean
-cd ../pseudo 
+pushd ../xdcomms-dma/pseudo
 sudo ./sue_donimous_unload
 make clean
-cd ../test
+popd
