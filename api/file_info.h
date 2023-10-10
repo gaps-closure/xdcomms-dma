87eebd828d744def3c9ec3aa31fbae23ae6ea79d
tmcauley <amcauley@peratonlabs.com>
2023-10-10 11:08:07 -0400

    xarb port env variable added

--- api/file_info.h
+++ api/file_info.h
@@ -11,12 +11,14 @@
 #define FILE_DIR_SHARE                1   // Flows share directory? 0=no, 1=yes
 #define XARBITOR_SEND_SCRIPT_FILENAME "./send-main"
 #define XARBITOR_IP_DEFAULT           "192.168.100.101"
-#define XARBITOR_SEND_SCRIPT_FIXED_ARGS  " -g 1124 -p /home/closure/XA-FILES/xarbitor/api/test_apps/etc/pki_auth -c /home/closure/XA-FILES/xarbitor/api/test_apps/etc/credential/xarbitor_temporary_1.0.0.0_client.p12 --password=password"
+#define XARBITOR_PORT_DEFAULT         "1124"
+#define XARBITOR_SEND_SCRIPT_FIXED_ARGS  " -p /home/closure/XA-FILES/xarbitor/api/test_apps/etc/pki_auth -c /home/closure/XA-FILES/xarbitor/api/test_apps/etc/credential/xarbitor_temporary_1.0.0.0_client.p12 --password=password"
 
 typedef struct _file_channel {
   int     pkt_index_last;
   int     pkt_index_next;
   char    xarb_IP[16];
+  char    xarb_port[16];
   uint8_t pkt_buffer[FILE_MAX_BYTES];
 
 } file_channel;
