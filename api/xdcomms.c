43b9487f19b333c53a23d5f8ea3d0eb6a4066978
tmcauley <amcauley@peratonlabs.com>
2023-10-18 14:56:52 -0400

    remove microsec traces

--- api/xdcomms.c
+++ api/xdcomms.c
@@ -216,7 +216,7 @@ void dma_rcvr(vchan *cp) {
 
   // A) Wait for Packet from DMA
   cbuf_ptr->length = sizeof(bw);            /* Receive up to make length Max size */
-  log_debug("THREAD-2 waiting for any tag on %s device %s: fd=%d max_len=%d cb_index=%d", cp->dev_type, cp->dev_name, cp->fd, cbuf_ptr->length, dma_cb_index);
+  log_trace("THREAD-2 waiting for any tag on %s device %s: fd=%d max_len=%d cb_index=%d", cp->dev_type, cp->dev_name, cp->fd, cbuf_ptr->length, dma_cb_index);
 #ifdef PRINT_US_TRACE
   time_trace("XDC_Rx1 DMA Waiting on %s cb_index=%d", cp->dev_name, dma_cb_index);
 #endif
